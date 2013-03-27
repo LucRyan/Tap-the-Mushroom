08cf8b0f66ca9a9486b99b7c8c03761f149fa3e1
Ryan <lucryan.w@gmail.com>
2013-03-27 02:17:39 -0700

    Add New Level for test.

---
+++ src/game/shake/ShakeItDown.hx
@@ -0,0 +1,16 @@
+package game.shake;
+import engine.scene.BaseScene;
+
+/**
+ * ...
+ * @author Yang Wang
+ */
+class ShakeItDown extends BaseScene
+{
+
+	public function new() 
+	{
+		super();
+	}
+	
+}
